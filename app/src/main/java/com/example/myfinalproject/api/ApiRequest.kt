9380640ec81930ec3996928aa1c568dcf35e2b76
Connor <resistingprawn5@gmail.com>
2024-11-24 15:11:33 -0700

    trying to implement api

---
+++ app/src/main/java/com/example/myfinalproject/api/ApiRequest.kt
@@ -0,0 +1,38 @@
+package com.example.myfinalproject.api
+
+import com.example.myfinalproject.order.Team
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+
+
+val apiKey = "050f76fd2b784f7db88f162dd4932831"
+
+const val api = "https://api.sportsdata.io/v3/nfl/scores/json/Standings/2023?key=050f76fd2b784f7db88f162dd4932831"
+
+
+
+//defining api service
+interface ApiService {
+    @GET(api) //api endpoint
+    suspend fun getTeams(): List<Team>
+}
+
+//create Retrofit Instance
+object RetrofitClient {
+    private const val BASE_URL = "https://your-api-base-url.com/" // Replace with your API base URL
+
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
+
+
--- app/src/main/java/com/example/myfinalproject/order/DataSource.kt
+++ app/src/main/java/com/example/myfinalproject/order/DataSource.kt
@@ -1,6 +1,17 @@
 package com.example.myfinalproject.order
 
-data class Player(
-    val name: String,
-    val
+data class Team(
+    val teamName: String,
+    val conference: String,
+    val division: String,
+    val abbrev: String,
+    val wins: Int,
+    val losses: Int,
+    val ties: Int,
+    val winningPercentage: Float,
+    val pointsFor: Int, //points scored
+    val pointsAgainst: Int, //points given up
+    val divisionRank: Int,
+    val conferenceRank: Int,
+
 )
